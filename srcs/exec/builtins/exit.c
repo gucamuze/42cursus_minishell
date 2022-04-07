ac7b6fcb59ff209765ecfee846dd560ebe306cc1
Maeva ALBRAND <malbrand@e3r2p12.clusters.42paris.fr>
2022-04-07 11:02:00 +0200

    norminette exit

--- srcs/exec/builtins/exit.c
+++ srcs/exec/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: gucamuze <gucamuze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 15:34:08 by malbrand          #+#    #+#             */
-/*   Updated: 2022/04/07 04:53:00 by gucamuze         ###   ########.fr       */
+/*   Updated: 2022/04/07 10:59:51 by malbrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +47,46 @@ static unsigned long long int	ft_atoul(const char *nptr)
 	return (sign * result);
 }
 
+static void	exit_num(t_command *cmd, long long int l_nb,
+				unsigned long long int nb)
+{
+	if (cmd->args[1][0] != '-')
+	{
+		nb = ft_atoul(cmd->args[1]);
+		if (nb <= 255)
+			g_exit = nb;
+		else if (nb <= 9223372036854775807)
+			g_exit = nb % 256;
+		else
+		{
+			g_exit = 2;
+			printf(": %s: numeric argument required", cmd->args[1]);
+		}
+	}
+	else if (cmd->args[1][0] == '-')
+	{
+		l_nb = ft_atol(cmd->args[1]);
+		if (l_nb < 0)
+			g_exit = l_nb % 256;
+		else
+		{
+			g_exit = 2;
+			printf(": %s: numeric argument required", cmd->args[1]);
+		}
+	}
+}
+
+static void	free_exit(t_command *cmd, t_data *data)
+{
+	free_exit(cmd, data);
+	cleanup(data);
+	close_all_fds(cmd);
+	cmd_lst_free(cmd);
+	exit(g_exit);
+}
+
 unsigned int	ft_exit(t_command *cmd, t_data *data, int mode)
 {
-	long long int			l_nb;
-	unsigned long long int	nb;
-
 	if (mode == 0)
 	{
 		printf("exit");
@@ -64,31 +99,7 @@ unsigned int	ft_exit(t_command *cmd, t_data *data, int mode)
 				printf(": too many arguments\n");
 				return (1);
 			}
-			if (cmd->args[1][0] != '-')
-			{
-				nb = ft_atoul(cmd->args[1]);
-				if (nb <= 255)
-					g_exit = nb;
-				else if (nb <= 9223372036854775807)
-					g_exit = nb % 256;
-				else
-				{
-					g_exit = 2;
-					printf(": %s: numeric argument required", cmd->args[1]);
-				}
-			}
-			else if (cmd->args[1][0] == '-')
-			{
-				l_nb = ft_atol(cmd->args[1]);
-				printf("\n%lld\n\n", l_nb);
-				if (l_nb < 0)
-					g_exit = l_nb % 256;
-				else
-				{
-					g_exit = 2;
-					printf(": %s: numeric argument required", cmd->args[1]);
-				}
-			}
+			exit_num(cmd, 0, 0);
 		}
 		else
 		{
@@ -96,10 +107,6 @@ unsigned int	ft_exit(t_command *cmd, t_data *data, int mode)
 			g_exit = 2;
 		}
 		printf("\n");
-		cleanup(data);
-		close_all_fds(cmd);
-		cmd_lst_free(cmd);
-		exit(g_exit);
 	}
 	return (0);
 }
